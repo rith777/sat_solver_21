b3340097e3058dd1c1c7a979edb9091d3f400926
Francieli Viane <francielifrv@gmail.com>
2024-11-10 11:03:02 +0100

    Outputs pretty sudoky matrix for easier validation

--- Scripts/DPLL_no_hueristics.py
+++ Scripts/DPLL_no_hueristics.py
@@ -1,88 +1,90 @@
 import sys
 
+
 def load_my_file(path):
     """load a DIMACS file and grab the clauses and var count."""
     clauses_list = []
     with open(path, 'r') as f:
         for line in f:
-            #organizing 4 columns p cnf var_count clause_count
+            # organizing 4 columns p cnf var_count clause_count
             if line.startswith('p cnf'):
                 _, _, var_count, clause_count = line.split()
                 var_count, clause_count = int(var_count), int(clause_count)
             else:
                 clause = list(map(int, line.strip().split()))[:-1]
-                clauses_list.append(clause) #add clause to list of clauses
-    return clauses_list, var_count #return clause and var count
+                clauses_list.append(clause)  # add clause to list of clauses
+    return clauses_list, var_count  # return clause and var count
+
 
 def unit_propagate(clauses, assign):
     """simplify clauses with unit propagation."""
     has_changes = True
-    while has_changes: #run until changes are found in list of clauses.
+    while has_changes:  # run until changes are found in list of clauses.
         has_changes = False
-        unit_clauses = [] #hold clauses with one literal
+        unit_clauses = []  # hold clauses with one literal
 
-        #find unit clauses
+        # find unit clauses
         for c in clauses:
             if len(c) == 1:
-                unit_clauses.append(c[0]) #adds all clauses with one var (or unit clauses)
+                unit_clauses.append(c[0])  # adds all clauses with one var (or unit clauses)
 
-        #processing unit clauses
+        # processing unit clauses
         for unit in unit_clauses:
-            assign[abs(unit)] = unit > 0  #setting var in unit clause to true or false
+            assign[abs(unit)] = unit > 0  # setting var in unit clause to true or false
 
-            new_clauses = [] #post unit clauses collection of clauses
+            new_clauses = []  # post unit clauses collection of clauses
             for c in clauses:
-                if unit not in c: #add clauses disatisfying with unit clause
+                if unit not in c:  # add clauses disatisfying with unit clause
                     new_clauses.append(c)
             clauses = new_clauses
 
             # Remove negation of unit clauses too (ex: removing p and not(p) if p is a literal)
             for clause in clauses:
-                if -unit in clause: #if negated unit clause in clause you can remove it from the clause
+                if -unit in clause:  # if negated unit clause in clause you can remove it from the clause
                     clause.remove(-unit)
-            has_changes = True # revert boolean to say change has been made.
-    return clauses, assign #return assignment and updated clauses without literal
+            has_changes = True  # revert boolean to say change has been made.
+    return clauses, assign  # return assignment and updated clauses without literal
 
-def eliminate_pure_lit(clauses, assign):
+
+def eliminate_pure_literal(clauses, assign):
     """assign values to pure literals"""
-    all_lits = []
+    literals = []
     for c in clauses:
-        for lit in c:
-            all_lits.append(lit) #all literals from all clauses
+        for literal in c:
+            literals.append(literal)  # all literals from all clauses
 
     # identifying pure lits and assigning truth valeus
-    for lit in set(all_lits):
-        if -lit not in all_lits:  # if negation absent it is pure (ex if p then not(p) should not be in lit for p to be pure)
-            assign[abs(lit)] = lit > 0  # Set true or false for positive or negative pure lit
+    for literal in set(literals):
+        if -literal not in literals:  # if negation absent it is pure (ex if p then not(p) should not be in lit for p to be pure)
+            assign[abs(literal)] = literal > 0  # Set true or false for positive or negative pure lit
             # if it's a positive pure literal (ex p), we can assign it true
             # If it's a negative pure literal (ex, ¬p), we can assign its (p) to False
 
             new_clauses = []
-            for c in clauses: #basically discards all clauses with the pure literal returns updated list of clauses
-                if lit not in c:
+            for c in clauses:  # basically discards all clauses with the pure literal returns updated list of clauses
+                if literal not in c:
                     new_clauses.append(c)
             clauses = new_clauses
     return clauses, assign
+
 
 def dpll_algorithm(clauses, assign):
     """DPLL algorithm"""
 
     # do unit propagation and pure lit elimination
     clauses, assign = unit_propagate(clauses, assign)
-    clauses, assign = eliminate_pure_lit(clauses, assign)
-
+    clauses, assign = eliminate_pure_literal(clauses, assign)
 
     if not clauses:  # All clauses satisfied so return true (solution found!!!)
         return True, assign
     if any(len(c) == 0 for c in clauses):  # Found an unsatisfiable clause
         return False, {}
 
-
-    unassigned_vars = [] #makes sure all vars are only picked once
-    for c in clauses: #check all clauses
-        for lit in c: #check all literals
-            if abs(lit) not in assign: #check if the absolute value of the literal is not in the assigned variables
-                unassigned_vars.append(abs(lit)) #add abs value of var to unassigned_vars
+    unassigned_vars = []  # makes sure all vars are only picked once
+    for c in clauses:  # check all clauses
+        for lit in c:  # check all literals
+            if abs(lit) not in assign:  # check if the absolute value of the literal is not in the assigned variables
+                unassigned_vars.append(abs(lit))  # add abs value of var to unassigned_vars
     if not unassigned_vars:  # if empty then it should be unsatisfiable
         return False, {}
 
@@ -92,27 +94,49 @@ def dpll_algorithm(clauses, assign):
     updated_assign = assign.copy()
     updated_assign[selected_var] = True
 
-    #recursive call with updated assignment of var and filter out clauses that selected var satisfies
+    # recursive call with updated assignment of var and filter out clauses that selected var satisfies
     sat, result = dpll_algorithm([c for c in clauses if selected_var not in c],
-                                        updated_assign)
-    if sat: #if satisfiable return true
+                                 updated_assign)
+    if sat:  # if satisfiable return true
         return True, result
 
     # update assignment var as false and try the same recursive call
     updated_assign[selected_var] = False
     sat, result = dpll_algorithm([c for c in clauses if -selected_var not in c],
-                                        updated_assign)
+                                 updated_assign)
     return sat, result
+
 
 def save_my_output(path, assign, satisfiable):
     """Write result to output file (ex filename.out)."""
     output_file = path + '.out'
-    with open(output_file, 'w') as output:
-        if satisfiable:
-            for key, value in assign.items():
-                output.write(f"{key if value else -key} 0\n")
-        else:
-            output.write("")   # Write empty file for unsatisfiable
+    sudoku_path = path.replace(".cnf", ".txt")
+    if satisfiable:
+        with open(output_file, 'w') as output:
+            output.write("0\n".join(convert_to_cnf(assign)))
+
+        with open(sudoku_path, mode='w') as sudoku:
+            sudoku.write(pretty_matrix(convert_to_matrix(assign)))  # Write empty file for unsatisfiable
+
+
+def convert_to_cnf(results):
+    return [str(key) if value else str(-key) for key, value in results.items()]
+
+
+def convert_to_matrix(matrix):
+    sudoku = [[0 for _ in range(9)] for _ in range(9)]
+    for key, value in matrix.items():
+        if value:
+            row, column, value = list(str(key).strip())
+            sudoku[int(row) - 1][int(column) - 1] = int(value)
+
+    return sudoku
+
+
+def pretty_matrix(matrix):
+    return '\n'.join(['\t'.join([str(cell) for cell in row]) for row in matrix])
+
+
 def main_flow():
     if len(sys.argv) != 3:
         print("Usage: python DPLL_no_heuristics.py <rulesfilepath> <puzzlefilepath>")
@@ -138,6 +162,7 @@ def main_flow():
         print("SATISFIABLE")
     else:
         print("UNSATISFIABLE")
+
 
 if __name__ == "__main__":
     main_flow()
